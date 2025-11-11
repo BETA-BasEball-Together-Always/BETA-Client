2913c11ade6a8770b025198a9bc6cf52d377b2b8
bmbx08 <bmbx08@bu.ac.kr>
2025-11-11 19:26:50 +0900

    feat/nativewind 적용 및 UI 더미로 확인 완료

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
@@ -7,4 +8,5 @@ config.transformer.babelTransformerPath = require.resolve('react-native-svg-tran
 config.resolver.assetExts = config.resolver.assetExts.filter((ext) => ext !== 'svg');
 config.resolver.sourceExts = [...config.resolver.sourceExts, 'svg'];
 
-module.exports = config;
+// module.exports = config;
+module.exports = withNativeWind(config, { input: './global.css' })
