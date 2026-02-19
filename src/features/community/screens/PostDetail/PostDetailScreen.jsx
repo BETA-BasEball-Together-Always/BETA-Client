756bb5f405469b3dd72900c5b6634396221851d8
Khaneul1 <kosky1219@gmail.com>
2026-02-20 01:47:06 +0900

    feat: 게시글 상세 리액션 바 및 레이아웃 개선
    - 리액션 바 UI 추가
    - 길게 누르기 시 리액션 선택 기능 구현
    - 선택된 리액션 상태 및 개수 관리 로직 추가
    - 공감 수 / 댓글 수 가로 정렬 수정
    - AppHeader 일부 레이아웃 수정
    - package-lock.json 변경 반영
    
    TODO:
    - 리액션 바 모달창 또는 팝업창으로 분리 (게시글 가리지 않게 할 것)
    - 리액션 바 + 공감 및 댓글 총 개수 + 좋아요/댓글/URL 복사 아이콘 컴포넌트 분리
    - 단일 탭 시 EMO_JOY 기본값 설정
    - 공감 총 개수가 0일 경우 댓글 수만 렌더링 처리
    - 같은 공감이 하나 이상 달릴 경우 이모티콘 리스트에 하나씩 추가 (총 4개의 이모티콘)

--- src/features/community/screens/PostDetail/PostDetailScreen.jsx
+++ src/features/community/screens/PostDetail/PostDetailScreen.jsx
@@ -1,16 +1,142 @@
-import React from "react";
-import { View, StyleSheet, ScrollView, Image } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, ScrollView, Image, Dimensions, TouchableOpacity, Modal } from "react-native";
 import { AppText } from "../../../../shared/theme/components/AppText";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useNavigation } from "@react-navigation/native";
+import AppHeader from "../../../../shared/component/AppHeader";
+// import * as Clipboard from "expo-clipboard";
 
-const PostDetailScreen = ({ route }) => {
-  const { post } = route.params; // PostDetailScreen으로 navigation할 때 post 데이터를 전달받는다고 가정
+import BackIcon from "../../../../shared/assets/svg/chevrons/back.svg";
+import MenuIcon from "../../assets/svg/TopBar/menuIcon.svg";
+
+import HeartIcon from "../../assets/svg/CommunityPost/heartIcon.svg"
+import HeartFilledIcon from "../../assets/svg/CommunityPost/heartFilledIcon.svg"
+import CommentIcon from "../../assets/svg/CommunityPost/commentIcon.svg";
+import CommentOnPressIcon from "../../assets/svg/CommunityPost/commentOnPressIcon.svg";
+import LinkIcon from "../../assets/svg/CommunityPost/linkIcon.svg";
+import LinkOnPressIcon from "../../assets/svg/CommunityPost/linkOnPressIcon.svg";
+
+const { width } = Dimensions.get("window");
+
+const PostDetailScreen = ({ route, navigation }) => {
+  const { post, onSelectReaction } = route.params; // PostDetailScreen으로 navigation할 때 post 데이터를 전달받는다고 가정
   console.log("PostDetailScreen에서 받은 post 데이터:", post);
+
+  const imageList = post.images ?? (post.image ? [post.image] : []);
+  const reactions = [
+    { id: "EMO_JOY", emoji: "💖", bgColor: "#FFBDBD" },
+    { id: "EMO_SAD", emoji: "😭", bgColor: "#C2EFFF" },
+    { id: "EMO_FUN", emoji: "🤣", bgColor: "#FFFABF" },
+    { id: "EMO_HYPE", emoji: "🔥", bgColor: "#FF9F76" },
+  ];
+
+
+  // 각 아이콘 버튼들 상태!!
+  const [selectedReaction, setSelectedReaction] = useState(null);
+  const [showReactionPicker, setShowReactionPicker] = useState(false);
+  const [reactionCounts, setReactionCounts] = useState(
+    post.reactionCounts || {
+      EMO_JOY: 0,
+      EMO_SAD: 0,
+      EMO_FUN: 0,
+      EMO_HYPE: 0,
+    }
+  );
+
+  const totalReactions = Object.values(reactionCounts).reduce(
+    (sum, val) => sum + val,
+    0
+  );
+
+  const [commentMode, setCommentMode] = useState(false);
+  const [linkPressed, setLinkPressed] = useState(false);
+  const [copyModalVisible, setCopyModalVisible] = useState(false);
+
+  const handleMorePress = () => {
+    if (post.isMine) {
+      console.log("수정/삭제 모달");
+    } else {
+      console.log("신고 모달");
+    }
+  };
+
+  const handleLikePress = () => {
+    // setShowReactionPicker((prev) => !prev);
+    setShowReactionPicker(!showReactionPicker);
+  };
+
+  const handleReactionSelect = (reaction) => {
+      const prevReaction = selectedReaction;
+
+    if (prevReaction?.id === reaction.id) {
+    setReactionCounts((prevCounts) => ({
+      ...prevCounts,
+      [reaction.id]: Math.max(prevCounts[reaction.id] - 1, 0),
+    }));
+
+    setSelectedReaction(null);
+    setShowReactionPicker(false);
+
+    if (onSelectReaction) {
+      onSelectReaction(post.id, null);
+    }
+
+    return;
+  }
+    if (prevReaction) {
+    setReactionCounts((prevCounts) => ({
+      ...prevCounts,
+      [prevReaction.id]: Math.max(prevCounts[prevReaction.id] - 1, 0),
+    }));
+  }
+
+    setReactionCounts((prevCounts) => ({
+      ...prevCounts,
+      [reaction.id]: prevCounts[reaction.id] + 1,
+    }));
+
+    setSelectedReaction(reaction);
+    setShowReactionPicker(false);
+
+    if (onSelectReaction) {
+      onSelectReaction(post.id, reaction);
+    }
+  };
+
+  const handleCommentPress = () => {
+    setCommentMode(!commentMode);
+  }
+
+  const handleCopyLink = async () => {
+    await Clipboard.setStringAsync(post.uri ?? "https://example.com");
+    setLinkPressed(true);
+    setCopyModalVisible(true);
+
+    setTimeout(() => {
+      setLinkPressed(false);
+      setCopyModalVisible(false);
+    }, 1500)
+  }
 
   return (
     <SafeAreaView style={styles.safeArea} edges={["top", "left", "right"]}>
+      <AppHeader
+      left = {
+        <TouchableOpacity style={styles.backButton}
+        onPress={() => navigation.goBack()}>
+          <BackIcon width={12} height={18.5} />
+          <AppText variant="bodyRegular" style={styles.backLabel}>뒤로가기</AppText>
+        </TouchableOpacity>
+      }
+      right = {
+        <TouchableOpacity onPress={handleMorePress}>
+          <MenuIcon width={20} height={20} />
+        </TouchableOpacity>
+      }
+      />
+
       <ScrollView contentContainerStyle={styles.container}>
+        {/* 유저 프로필!! 기찬 오빠 코드에서 따왔습니다! 이 부분은 수정 예정*/}
         <View style={styles.header}>
           <View style={styles.avatarCircle}>
             <AppText variant="middle" className="text-white">
@@ -19,52 +145,148 @@ const PostDetailScreen = ({ route }) => {
           </View>
 
           <View style={styles.headerText}>
-            <AppText
-              variant="semi14"
-              className="text-gray-400"
-              style={styles.nickname}
-            >
+            <AppText variant="semi14" style={styles.nickname}>
               {post.nickname}
             </AppText>
-            <AppText variant="bodyRegular" className="text-gray-500">
+            <AppText variant="numMediumRegular" className="text-gray-500">
               {post.timeAgo}
             </AppText>
           </View>
         </View>
 
-        {/* 본문 내용 */}
-        {post.image ? (
-          <View style={styles.imageWrapper}>
-            <Image
-              source={{ uri: post.image }}
-              style={styles.postImage}
-              resizeMode="cover"
-            />
-          </View>
-        ) : (
+        {imageList.length > 0 && (
+          <ScrollView
+            horizontal
+            pagingEnabled
+            showsHorizontalScrollIndicator={false}
+            style={styles.imageScroll}
+          >
+            {imageList.map((img, index) => (
+              <View key={index} style={styles.imageWrapper}>
+                <Image
+                  source={{ uri: img }}
+                  style={styles.postImage}
+                  resizeMode="cover"
+                />
+              </View>
+            ))}
+          </ScrollView>
+        )}
+        {post.content && (
           <View style={styles.textWrapper}>
-            <AppText variant="bodyRegular" className="text-white">
+            <AppText variant="middle" style={styles.content}>
               {post.content}
             </AppText>
           </View>
         )}
-        {post.image && (
-          <View style={styles.textWrapper}>
-            <AppText variant="bodyRegular" className="text-white">
-              {post.content}
+
+        {!showReactionPicker && (
+          <View style={styles.reactionSummary}>
+            <View style={styles.reactionIconRow}>
+              {reactions.map((reaction) => {
+                if (reactionCounts[reaction.id] > 0) {
+                  return (
+                    <View key={reaction.id}
+                    style={[
+                      styles.summaryCircle,
+                      {backgroundColor: reaction.bgColor},
+                    ]}
+                    >
+                      <AppText style={styles.summaryEmoji}>
+                        {reaction.emoji}
+                      </AppText>
+                    </View>
+                  );
+                }
+                return null;
+              })}
+
+              <AppText style={styles.summaryCount}>
+                {totalReactions}
+              </AppText>
+            </View>
+            <AppText variant="numMediumRegular" className="text-gray-400">
+              댓글 {post.comments ?? 0}
             </AppText>
           </View>
         )}
+
+        <View style={styles.actionContainer}>
+          {showReactionPicker && (
+          <View style={styles.reactionBar}>
+            {reactions.map((reaction) => {
+              const isSelected = 
+                selectedReaction?.id === reaction.id;
+
+              return (
+                <TouchableOpacity
+                  key={reaction.id}
+                  onPress={() => handleReactionSelect(reaction)}
+                >
+                  <View style={[
+                    styles.reactionCircle,
+                    {backgroundColor: reaction.bgColor},
+                    selectedReaction && !isSelected && styles.dimmed,
+                  ]}>
+                    <AppText style={styles.reactionEmoji}>
+                      {reaction.emoji}
+                    </AppText>
+                  </View>
+                </TouchableOpacity>
+              );
+            })}
+          </View>
+        )}
+        
+        <View style={styles.actionRow}>
+          
+          <View style={styles.leftActions}>
+            {/* 좋아요 */}
+            <TouchableOpacity onPress={handleLikePress}
+            onLongPress={() => setShowReactionPicker(true)}
+            activeOpacity={0.7}>
+              {selectedReaction ? (
+                <HeartFilledIcon width={31.3} height={27} />
+              ) : (
+                <HeartIcon width={31.3} height={27} />
+              )}
+            </TouchableOpacity>
+
+            {/* 댓글 */}
+            <TouchableOpacity onPress={() => setCommentMode(!commentMode)}>
+              {commentMode ? (
+                <CommentOnPressIcon width={25} height={25} />
+              ) : (
+                <CommentIcon width={25} height={25} />
+              )}
+            </TouchableOpacity>
+          </View>
+
+          {/* URL 복사 */}
+          <TouchableOpacity onPress={handleCopyLink}>
+            {linkPressed ? (
+              <LinkOnPressIcon width={22} height={22} />
+            ) : (
+              <LinkIcon width={22} height={22} />
+            )}
+          </TouchableOpacity>
+        </View>
+
+        </View>
       </ScrollView>
+
+      <Modal transparent visible={copyModalVisible} animationType="fade">
+        <View style={styles.modalContainer}>
+          <View style={styles.modalBox}>
+            <AppText variant="middle">URL이 클립</AppText>
+          </View>
+        </View>
+      </Modal>
     </SafeAreaView>
   );
 };
 
 export default PostDetailScreen;
-
-const CARD_BG = "#1C1C1E";
-const TEXT_MAIN = "#F4F4F5";
-const TEXT_SUB = "#A1A1AA";
 
 const styles = StyleSheet.create({
   safeArea: {
@@ -72,13 +294,20 @@ const styles = StyleSheet.create({
     backgroundColor: "#121212",
   },
 
-  container: {
+  backButton: {
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  backLabel: {
+    color: "#F9F9F9",
+    marginLeft: 15,
+  },
+
+   container: {
     paddingHorizontal: 16,
     paddingTop: 12,
     paddingBottom: 40,
   },
-
-  /* 상단 작성자 영역 */
   header: {
     flexDirection: "row",
     alignItems: "center",
@@ -86,46 +315,43 @@ const styles = StyleSheet.create({
   },
 
   avatarCircle: {
-    width: 36,
-    height: 36,
-    borderRadius: 18,
+    width: 39.38,
+    height: 38,
+    borderRadius: 50,
     backgroundColor: "#27272A",
     justifyContent: "center",
     alignItems: "center",
   },
 
-  avatarInitial: {
-    color: "#fff",
-    fontSize: 16,
-    fontWeight: "700",
-  },
-
   headerText: {
     marginLeft: 10,
+    alignItems: "flex-start",
   },
 
   nickname: {
-    color: TEXT_MAIN,
-    fontSize: 14,
-    fontWeight: "600",
+    color: "#D4D4D4",
+    marginBottom: 2,
   },
 
   timeAgo: {
-    color: TEXT_SUB,
+    color: "#A1A1AA",
     fontSize: 11,
     marginTop: 2,
   },
 
   /* 이미지 */
-  imageWrapper: {
-    width: "100%",
-    aspectRatio: 3 / 2,
-    borderRadius: 16,
-    overflow: "hidden",
+  imageScroll: {
     marginBottom: 16,
   },
+  imageWrapper: {
+    width: width - 32, // 화면 너비에서 양쪽 패딩(16 + 16)을 뺀 값
+    aspectRatio: 3 / 2,
+    borderRadius: 10,
+    overflow: "hidden",
+    marginRight: 12,
+  },
 
-  image: {
+  postImage: {
     width: "100%",
     height: "100%",
   },
@@ -134,15 +360,85 @@ const styles = StyleSheet.create({
   textWrapper: {
     marginTop: 4,
   },
-
   content: {
-    color: TEXT_MAIN,
+    color: "#F9F9F9",
     fontSize: 15,
     lineHeight: 22,
   },
-  postImage: {
-    width: "100%",
-    height: "100%",
-    borderRadius: 10,
+  actionRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 14,
+    marginHorizontal: 5,
   },
+  leftActions: {
+    flexDirection: "row",
+    gap: 25,
+  },
+  // modalContainer: {
+  //   flex: 1,
+  //   justifyContent: "center",
+  //   alignItems: "center",
+  //   backgroundColor: "rgba(0,0,0,0.5)",
+  // },
+  // modalBox: {
+  //   backgroundColor: "#FFFFFF",
+  //   padding: 20,
+  //   borderRadius: 10,
+  // }
+  actionContainer: {
+    marginTop: 20,
+  },
+  reactionBar: {
+    flexDirection: "row",
+    backgroundColor: "#D9D9D9",
+    borderRadius: 30,
+    paddingVertical: 7,
+    alignSelf: "flex-start",
+  },
+  reactionCircle: {
+    width: 50,
+    height: 48,
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: "center",
+    marginHorizontal: 6,
+  },
+  reactionEmoji: {
+    fontSize: 18,
+  },
+  dimmed: {
+    opacity: 0.3
+  },
+
+  reactionSummary: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 14,
+  },
+  reactionIconRow: {
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  summaryCircle: {
+    width: 26,
+    height: 26,
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 4,
+  },
+
+  summaryEmoji: {
+    fontSize: 14,
+  },
+
+  summaryCount: {
+    color: "#F9F9F9",
+    marginLeft: 6,
+    fontSize: 14,
+  },
+  
 });
--- src/shared/component/AppHeader.jsx
+++ src/shared/component/AppHeader.jsx
@@ -1,81 +1,48 @@
 import React from "react";
 import { View, TouchableOpacity, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import { SafeAreaView } from "react-native-safe-area-context";
 import BackIcon from "../assets/svg/chevrons/back.svg";
 import { AppText } from "../theme/components/AppText";
 
-const AppHeader = ({ pageName, showBack = false, backLabel }) => {
+const AppHeader = ({ left, center, right }) => {
   const navigation = useNavigation();
 
   return (
-    // <SafeAreaView edges={["top"]} style={styles.safe}>
     <View style={styles.navContainer}>
-      {showBack && (
-        <TouchableOpacity
-          style={styles.backButton}
-          onPress={() => navigation.goBack()}
-        >
-          <BackIcon width={12} height={18} marginTop={4} />
-          {/* backLabel은 게시글 상세 페이지에서 "< 뒤로가기" 할 때 필요!!" */}
-          {backLabel && (
-            <AppText variant="bodyRegular" style={styles.backLabel}>
-              {backLabel}
-            </AppText>
-          )}
-        </TouchableOpacity>
-      )}
-
-      {pageName && (
-        <View style={styles.pageNameContainer}>
-          <AppText variant="displayTitle2" className="text-white">
-            {pageName}
-          </AppText>
-        </View>
-      )}
-
-      {/* 오른쪽 균형용 빈 공간 */}
-      {/* <View style={styles.right} /> */}
+      <View style={styles.left}>{left}</View>
+      <View style={styles.center}>{center}</View>
+      <View style={styles.right}>{right}</View>
     </View>
-    // </SafeAreaView>
   );
 };
 
 export default AppHeader;
 
 const styles = StyleSheet.create({
-  safe: {
-    backgroundColor: "#121212",
-  },
   navContainer: {
     height: 56,
-    // flexDirection: "row",
-    // alignItems: "center",
-    // justifyContent: "flex-end",
     justifyContent: "center",
     paddingHorizontal: 25,
   },
-  backButton: {
+  left: {
     position: "absolute",
     left: 25,
     height: "100%",
-    flexDirection: "row",
-    alignItems: "center",
+    justifyContent: "center",
   },
-  backLabel: {
-    color: "#FFFFFF",
-    marginLeft: 19,
-    alignItems: "center",
-  },
-  pageNameContainer: {
+  center: {
     position: "absolute",
     left: 0,
     right: 0,
-    justifyContent: "center",
     alignItems: "center",
+    justifyContent: "center",
     height: "100%",
   },
-  //   right: {
-  //     width: 24,
-  //   },
+  right: {
+    position: "absolute",
+    right: 25,
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "flex-end",
+  },
 });
