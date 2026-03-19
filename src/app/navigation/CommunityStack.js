c0c3e0eaf94eaf76663f6d7c8a74de5929d97986
Khaneul1 <kosky1219@gmail.com>
2026-03-19 16:02:34 +0900

    코드 병합 에러 수정

--- src/app/navigation/CommunityStack.js
+++ src/app/navigation/CommunityStack.js
@@ -3,43 +3,28 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 // import PostDetailScreen from "@features/community/screens/PostDetail/PostDetailScreen";
 import CreatePostScreen from "../../features/community/screens/CreatePost/CreatePostScreen";
-<<<<<<< HEAD
-import UploadSuccessScreen from "../../features/community/screens/CreatePost/UploadSuccessScreen";
 import TeamCommunityScreen from "../../features/community/screens/TeamCommunityScreen";
 import AllCommunityScreen from "../../features/community/screens/AllCommunityScreen";
-=======
 import CreatePostCameraScreen from "../../features/community/screens/CreatePost/CreatePostCameraScreen";
 import UploadSuccessScreen from "../../features/community/screens/CreatePost/UploadSuccessScreen";
-// import TeamCommunityScreen from "../../features/community/screens/TeamCommunityScreen";
-// import AllCommunityScreen from "../../features/community/screens/AllCommunityScreen";
-import CommunityScreen from "../../features/community/screens/CommunityScreen";
 
->>>>>>> origin/feat/15-create-post-screen
 const Stack = createNativeStackNavigator();
 
 const CommunityStack = () => {
   return (
     <Stack.Navigator
       screenOptions={{ headerShown: false }}
-<<<<<<< HEAD
-      initialRouteName="TeamCommunity"
+      initialRouteName="AllCommunity"
     >
       <Stack.Screen name="TeamCommunity" component={TeamCommunityScreen} />
       <Stack.Screen name="AllCommunity" component={AllCommunityScreen} />
       {/* <Stack.Screen name="PostDetail" component={PostDetailScreen} /> */}
-      {/* <Stack.Screen name="CreatePost" component={CreatePostScreen} />
-      <Stack.Screen name="UploadSuccess" component={UploadSuccessScreen} /> */}
-=======
-      initialRouteName="Community"
-    >
-      {/* <Stack.Screen name="TeamCommunity" component={TeamCommunityScreen} />
-      <Stack.Screen name="AllCommunity" component={AllCommunityScreen} /> */}
-      <Stack.Screen name="Community" component={CommunityScreen} />
-      {/* <Stack.Screen name="PostDetail" component={PostDetailScreen} /> */}
       <Stack.Screen name="CreatePost" component={CreatePostScreen} />
-      <Stack.Screen name="CreatePostCamera" component={CreatePostCameraScreen} />
+      <Stack.Screen
+        name="CreatePostCamera"
+        component={CreatePostCameraScreen}
+      />
       <Stack.Screen name="UploadSuccess" component={UploadSuccessScreen} />
->>>>>>> origin/feat/15-create-post-screen
     </Stack.Navigator>
   );
 };
