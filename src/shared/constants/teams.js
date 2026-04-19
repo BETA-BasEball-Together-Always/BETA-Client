8164a66d8b6a44b8ce7533ba4bb65f9db10e7fd8
Khaneul1 <kosky1219@gmail.com>
2026-04-19 20:58:33 +0900

    style: 야구네컷 구단 선택 키움/두산 텍스트 로고 사이즈 조정

--- src/shared/constants/teams.js
+++ src/shared/constants/teams.js
@@ -90,7 +90,7 @@ export const TEAM_DATA = {
     label: "두산베어스",
     MainIcon: DOOSAN,
     ProfileIcon: PROFILE_DOOSAN,
-    iconScale: 1.15,
+    iconScale: 1.13,
     gradient: {
       colors: ["#4B6FA5", "#1A2E6E", "#0A1540", "#05091F"],
       locations: [0, 0.3, 0.65, 1],
@@ -122,7 +122,7 @@ export const TEAM_DATA = {
     label: "키움히어로즈",
     MainIcon: KIWOOM,
     ProfileIcon: PROFILE_KIWOOM,
-    iconScale: 1.15,
+    iconScale: 1.13,
     gradient: {
       colors: ["#C03050", "#8C1A2E", "#4A0818", "#1A0008"],
       locations: [0, 0.35, 0.7, 1],
